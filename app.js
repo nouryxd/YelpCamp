c40ddea220a2d21aa0d7a8d22016f1bcee3fd0e5
lyx0 <stefan@parfuss.me>
2021-07-14 15:33:49 +0200

    add campground show

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.get('/campgrounds', async (req, res) => {
    res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/show', { campground })
+})
+
 app.listen(8080, () => {
     console.log('Listening on port 8080')
 })
