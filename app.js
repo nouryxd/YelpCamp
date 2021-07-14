34612fe5518c59a60649bf01ea86065d7d27edb6
lyx0 <stefan@parfuss.me>
2021-07-14 15:00:04 +0200

    add campground index

--- app.js
+++ app.js
@@ -29,10 +29,9 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping' })
-    await camp.save()
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+   const campgrounds = await Campground.find({}) 
+   res.render('campgrounds/index', { campgrounds })
 })
 
 app.listen(8080, () => {
