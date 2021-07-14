d873c1e5b397617f59405ea0c48cbf11fc134d80
lyx0 <stefan@parfuss.me>
2021-07-14 15:58:48 +0200

    add creating campgrounds

--- app.js
+++ app.js
@@ -24,22 +24,37 @@ app.set('view engine', 'ejs');
 // app.js in it, regardless of where we call it
 app.set('views', path.join(__dirname, 'views'))
 
+// Enables us to read the request body from the
+// POST request
+app.use(express.urlencoded({extended: true}))
+
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('/campgrounds', async (req, res) => {
-   const campgrounds = await Campground.find({}) 
-   res.render('campgrounds/index', { campgrounds })
+   const campgrounds = await Campground.find({});
+   res.render('campgrounds/index', { campgrounds });
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id)
-    res.render('campgrounds/show', { campground })
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground });
 })
 
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
 app.listen(8080, () => {
-    console.log('Listening on port 8080')
+    console.log('Listening on port 8080');
 })
