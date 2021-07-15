b43bc24e36f1c3d95362571a79b3b73bdbcedee3
lyx0 <stefan@parfuss.me>
2021-07-15 21:25:35 +0200

    add more error handling

--- app.js
+++ app.js
@@ -62,6 +62,9 @@ app.get("/campgrounds/:id", catchAsync(async (req, res) => {
 }));
 
 app.post("/campgrounds", catchAsync(async (req, res, next) => {
+    if (!req.body.campground) {
+        throw new ExpressError('Invalid Campground Data', 400)
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -90,10 +93,17 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
     res.redirect("/campgrounds");
 }));
 
+// If no other specified url was hit this 
+// will catch every path and send a 404 message
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404 ))
+});
+
 // Really basic error handler that we will hit at 
 // the moment only from app.get new post
 app.use((err, req, res, next) => {
-    res.send('Something went wrong >:(')
+    const { statusCode = 500, message = "Something went wrong."} = err;
+    res.status(statusCode).send(message);
 })
 
 app.listen(8080, () => {
