296325345ebb7d67c82dd06cc9bfe61a3df0f0ea
lyx0 <stefan@parfuss.me>
2021-07-15 22:42:37 +0200

    add validation campgroundSchema

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 
+const Joi = require('joi');
+
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 
@@ -62,9 +64,17 @@ app.get("/campgrounds/:id", catchAsync(async (req, res) => {
 }));
 
 app.post("/campgrounds", catchAsync(async (req, res, next) => {
-    if (!req.body.campground) {
-        throw new ExpressError('Invalid Campground Data', 400)
-    }
+    // if (!req.body.campground) {
+    //     throw new ExpressError('Invalid Campground Data', 400)
+    // }
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+        }).required()
+    })
+    const result = campgroundSchema.validate(req.body)
+    console.log(result)
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
