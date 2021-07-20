f5da8c282e0f0b1952b7dded398d174060842c7c
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-07-20 19:52:07 +0200

    add defining view model

--- models/campground.js
+++ models/campground.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    title: String,
-    image: String,
-    price: Number,
-    description: String,
-    location: String,
+  title: String,
+  image: String,
+  price: Number,
+  description: String,
+  location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
