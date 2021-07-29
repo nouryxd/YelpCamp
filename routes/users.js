b282649d098fb8e134b01791186474c45426cd2c
lyx0 <stefan@parfuss.me>
2021-07-30 00:56:40 +0200

    add login routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
-
+const passport = require("passport");
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
@@ -26,4 +26,18 @@ router.post(
     }
   })
 );
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }),
+  (req, res) => {
+    req.flash("success", "Welcome back");
+    res.redirect("/campgrounds");
+  }
+);
+
 module.exports = router;
