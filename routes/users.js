3573b77c4682247d431325fd9b5e6ba6482bf26a
lyx0 <stefan@parfuss.me>
2021-07-30 00:25:49 +0200

    add register route logic

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
 
 const User = require("../models/user");
 
@@ -7,7 +8,22 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/register",
+  catchAsync(async (req, res, next) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to Yelp Camp!");
+        res.redirect("/campgrounds");
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
+  })
+);
 module.exports = router;
