01688495a5e63077a06be0caff04e5aa18414857
lyx0 <stefan@parfuss.me>
2021-07-29 23:33:57 +0200

    add register form

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
 // Routes
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -89,8 +90,9 @@ app.get("/fakeuser", async (req, res) => {
 });
 
 // Routes
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
