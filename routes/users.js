79c38df26ca1ebdbabfda825e07bba4be0ccc38d
lyx0 <stefan@parfuss.me>
2021-07-30 02:45:23 +0200

    log user in after he registered

--- routes/users.js
+++ routes/users.js
@@ -17,8 +17,11 @@ router.post(
             const registeredUser = await User.register(user, password);
             req.login(registeredUser, (err) => {
                 if (err) return next(err);
-                req.flash("success", "Welcome to Yelp Camp!");
-                res.redirect("/campgrounds");
+                req.login(registeredUser, (err) => {
+                    if (err) return next(err);
+                    req.flash("success", "Welcome to Yelp Camp!");
+                    res.redirect("/campgrounds");
+                });
             });
         } catch (e) {
             req.flash("error", e.message);
