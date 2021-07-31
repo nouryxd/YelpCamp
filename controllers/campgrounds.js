2089ca97d1a7637999e36fd1d92c52c33f58f33b
lyx0 <stefan@parfuss.me>
2021-08-01 00:40:26 +0200

    add view specific campground to controllers

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,12 +1,35 @@
 const Campground = require("../models/campground");
 
 // Index
+// http://localhost:8080/
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
 };
 
-// New
-module.exports.new = (req, res) => {
+// Create a new campground
+// http://localhost:8080/campgrounds/new
+module.exports.renderNewForm = (req, res) => {
     res.render("campgrounds/new");
+};
+
+// View a specific campground
+// http://localhost:8080/campgrounds/61035cc80b22ffed75596f00
+module.exports.view = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+        .populate({
+            path: "reviews",
+            populate: {
+                path: "author",
+            },
+        })
+        .populate("author");
+    // console.log(campground);
+    if (!campground) {
+        req.flash("error", "Campground not found");
+        return res.redirect("/campgrounds");
+    }
+    // console.log(campground);
+    res.render("campgrounds/show", { campground });
 };
