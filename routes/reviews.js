6abcf5faafd9b593beb52eff52fbe9be807f589d
lyx0 <stefan@parfuss.me>
2021-08-01 01:16:49 +0200

    create review controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -9,37 +9,12 @@ const { validateReview } = require("../middleware");
 const { isLoggedIn } = require("../middleware");
 const { isReviewAuthor } = require("../middleware");
 
-// Models
-const Review = require("../models/review");
-const Campground = require("../models/campground");
+const reviews = require("../controllers/reviews");
 
-router.post(
-    "/",
-    isLoggedIn,
-    validateReview,
-    catchAsync(async (req, res) => {
-        const campground = await Campground.findById(req.params.id);
-        const review = new Review(req.body.review);
-        review.author = req.user._id;
-        campground.reviews.push(review);
-        await review.save();
-        await campground.save();
-        req.flash("success", "Successfully posted a review.");
-        res.redirect(`/campgrounds/${campground._id}`);
-    })
-);
+// Create a new review
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
-router.delete(
-    "/:reviewId",
-    isLoggedIn,
-    isReviewAuthor,
-    catchAsync(async (req, res) => {
-        const { id, reviewId } = req.params;
-        await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-        await Review.findByIdAndDelete(reviewId);
-        req.flash("success", "Successfully deleted a review.");
-        res.redirect(`/campgrounds/${id}`);
-    })
-);
+// Delete a review
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
 module.exports = router;
