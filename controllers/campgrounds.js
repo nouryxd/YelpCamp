53f6e9eb0fa97bf77946bffdb4fd63907209dd2b
lyx0 <stefan@parfuss.me>
2021-08-04 04:03:14 +0200

    actually delete images

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -61,6 +61,12 @@ module.exports.updateCampground = async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
+    if (req.body.deleteImages) {
+        await campground.updateOne({
+            $pull: { images: { filename: { $in: req.body.deleteImages } } },
+        });
+        console.log(campground);
+    }
     await campground.save();
     req.flash("success", "Successfully updated campground!");
     res.redirect(`/campgrounds/${campground._id}`);
