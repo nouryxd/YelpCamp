cd6656f3060de29e9d69d6923c36951afad07a36
lyx0 <stefan@parfuss.me>
2021-08-05 22:12:25 +0200

    change map style, add maker popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: "map",
-    style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+    style: "mapbox://styles/mapbox/outdoors-v11", // stylesheet location
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 8, // starting zoom
 });
@@ -9,8 +9,8 @@ const map = new mapboxgl.Map({
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
-        new mapboxgl.Popup({ offset: 25 }).setHTML(
-            `<h3>${campground.title}</h3><p>${campground.location}</p>`
+        new mapboxgl.Popup({ offset: 20 }).setHTML(
+            `<h5>${campground.title}</h5><p>${campground.location}</p>`
         )
     )
     .addTo(map);
