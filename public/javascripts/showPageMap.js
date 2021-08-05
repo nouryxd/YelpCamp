f37c51b82fba5292521c57061e8e1c7388b0f2f3
lyx0 <stefan@parfuss.me>
2021-08-05 22:05:53 +0200

    change map style

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: "map",
-    style: "mapbox://styles/mapbox/light-v10", // stylesheet location
+    style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 8, // starting zoom
 });
