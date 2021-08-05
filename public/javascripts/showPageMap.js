e11186e14a76190a9faa1d6b64170643c9ee00f1
lyx0 <stefan@parfuss.me>
2021-08-06 01:49:16 +0200

    add controls to map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -6,6 +6,9 @@ const map = new mapboxgl.Map({
     zoom: 8, // starting zoom
 });
 
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
