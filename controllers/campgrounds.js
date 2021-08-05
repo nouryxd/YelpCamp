758033c845afc4d803631cd27a675d81cd7cdf2c
lyx0 <stefan@parfuss.me>
2021-08-05 20:24:14 +0200

    add geojson

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -27,8 +27,8 @@ module.exports.createCampground = async (req, res, next) => {
             limit: 1,
         })
         .send();
-    console.log(geoData.body.features[0].geometry.coordinates);
     const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
