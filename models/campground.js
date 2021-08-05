466bc860d90fdb225b35d35dd1d9cec2566d7f08
lyx0 <stefan@parfuss.me>
2021-08-05 23:48:42 +0200

    add custom popups to the map

--- models/campground.js
+++ models/campground.js
@@ -47,7 +47,9 @@ const CampgroundSchema = new Schema(
 );
 
 CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
-    return "POPUP TEXT";
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}<a/><strong>
+    <p>${this.description.substring(0, 40)}...</p>`;
 });
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
