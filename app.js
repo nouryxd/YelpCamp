79b5a42413c83ca0da4314dfb89063aa453091da
lyx0 <stefan@parfuss.me>
2021-08-06 03:44:38 +0200

    add mongo atlas

--- app.js
+++ app.js
@@ -23,8 +23,11 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
+const dbUri = process.env.MONGO_URI;
+
 // MongoDB
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+// mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+mongoose.connect(dbUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
